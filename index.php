4a8b9e96d7610748657a51b508496a7cfaff35e7
ravo1230 <98001912+ravo1230@users.noreply.github.com>
2022-01-19 08:42:46 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+if(isset($_GET['id'])) {
+$id = $_GET['id'];
+echo file_get_contents('https://1drv.ms/u/'.$id.' ');
+}   else {
+    echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Drive to Web Hosting</title>
+    <script src="https://bit.ly/drivetoweb2-anly"></script>
+</head>
+<body>
+    <form action="" method="get">
+     <input type="text" placeholder="Enter File ID " name="id">
+     <button type="submit">View</button>
+    </form>
+    <style>
+    input , button {
+        background : aqua ;
+        font-size : 25px ;
+        border : none ;
+        outline : none ;
+        padding : 4px ;
+    }
+    </style>
+</body>
+</html>';
+
+}
+
+?>
+
